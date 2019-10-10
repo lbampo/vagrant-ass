04614d77e72f50e9f602682e193b3641245e7786
Lennox Bampoe-Addo <LBampoe-Addo@spartaglobal.com>
2019-10-10 10:30:37 +0100

    Added files to be able to access nginx server

---
+++ environment/provision.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update -y
+
+sudo apt update -y
+
+sudo apt install nginx -y
+
+sudo apt-get install curl -y
+
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+sudo apt-get install nodejs -y
